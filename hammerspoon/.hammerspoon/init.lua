83a2fbb63b3e521492bedc6e93789e02eab16b4c
edieguez <dieguezemmanuel@gmail.com>
2025-04-02 09:49:24 -0600

    [hammerspoon] Remove support for fullscreen windows

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -6,9 +6,8 @@
 --
 -- Rules for focusing:
 -- 1. Prioritize focusing on the first non-fullscreen window in the current application stack.
--- 2. If no non-fullscreen windows are available, focus a fullscreen window (if one exists).
+-- 2. Minimized and hidden windows are not considered for focusing.
 -- 3. If all windows are minimized or no windows are left, focus Finder.
--- 4. Minimized windows are not considered for focusing.
 
 -- Initialize a window filter for all applications
 -- Ignore dialog windows and set sort order to focus on the last used window
@@ -22,21 +21,9 @@ local wf = hs.window.filter.new(nil)
 local function focusNextWindow()
     local windows = wf:getWindows()
 
-    local normalWindows = {}
-    local fullscreenWindows = {}
-
-    for _, win in ipairs(windows) do
-        if win:isFullScreen() then
-            table.insert(fullscreenWindows, win)
-        elseif not win:isMinimized() and not win:application():isHidden() then
-            table.insert(normalWindows, win)
-        end
-    end
-
-    if #normalWindows > 0 then
-        normalWindows[1]:focus()
-    elseif #fullscreenWindows > 0 then
-        fullscreenWindows[1]:focus()
+    if #windows > 0 then
+        -- Focus the first window in the list
+        windows[1]:focus()
     else
         -- If no windows are left, focus Finder
         hs.application.find("Finder"):activate()
